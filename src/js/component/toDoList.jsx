8e171a3127f20b61a83535b31d16e599e60a3909
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-05-24 22:12:21 +0000

    changes

--- src/js/component/toDoList.jsx
+++ src/js/component/toDoList.jsx
@@ -6,9 +6,23 @@ function ToDoList() {
         const [list, setList] = useState = {[]};
         const [input, setInput] =useState = ("");
 
+        const addTodo = (todo) => {
+            const newTodo = {
+             id: Math.randoom(),
+             todo: todo  
+            };
+
+        setList([...list, newTodo]);    
+        setInput("");
+
+        }
+
+
+
         return(
             <div class="input">
                 <input type="text" value="input" onchange={(e) => setInput(e.target.value)} />
+                <button.onClick = {() => addTodo}
             </div>   
         ) 
 };
