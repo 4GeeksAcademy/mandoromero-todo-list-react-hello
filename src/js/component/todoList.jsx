d6011148eba8bd7d75c27d4b11fbb152d6f3f9cf
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-05-29 18:07:16 +0000

    done

--- src/js/component/todoList.jsx
+++ src/js/component/todoList.jsx
@@ -31,10 +31,12 @@ const TodoList = () => {
                     <input
                         type="text"
                         value={input}
-                        placeholder="What to do?"
+                        placeholder="What needs to be done?"
                         size="45"
                         onChange={(e) => setInput(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter' && addTodo(input)}
                     />
+
                     <button className="add" onClick={() => addTodo(input)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
